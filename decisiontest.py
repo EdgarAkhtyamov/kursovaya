913854b2d214804410e35a4b94abd99340370161
EdgarAkhtyamov <114211215+EdgarAkhtyamov@users.noreply.github.com>
2024-05-13 00:06:45 +0300

    Update decisiontest.py

--- decisiontest.py
+++ decisiontest.py
@@ -1,5 +1,43 @@
 import math
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import pandas
+import ast
 
+
+data = pandas.read_csv("dataset.csv", sep=';')
+
+
+print(data.head())
+
+x = data["Matrix"].apply(ast.literal_eval)
+
+
+x = x.apply(lambda row: [list(map(int, sublist)) for sublist in row])
+
+flattened_matrices = [np.array(matrix).flatten() for matrix in x]
+
+X = np.array(flattened_matrices)
+
+y = data['Res']
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=4)
+clf = clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+print("Accuracy on training set: {:.3f}".format(clf.score(x_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(clf.score(x_test, y_test)))
+
+
+
+
+
+
+'''
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
@@ -38,3 +76,4 @@ with open('dataset.csv', 'r') as dataset:
     print("Accuracy on training set: {:.3f}".format(clf.score(x_train, y_train)))
     print("Accuracy on test set: {:.3f}".format(clf.score(x_test, y_test)))
 
+'''
